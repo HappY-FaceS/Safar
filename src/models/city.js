ff82ebf04e04c974a330f9f09869dbc904426203
HappY-FaceS <rishavkumarcgc@gmail.com>
2023-03-11 20:11:09 +0530

    added airports db model-migration

--- src/models/city.js
+++ src/models/city.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      // [1:many] relation between City:Airport
+      this.hasMany(models.Airport, {
+        foreignKey: 'cityId'
+      });
     }
   }
   City.init({
