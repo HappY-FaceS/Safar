b73deef579ce046cf74b32fbed6556b6852f248b
HappY-FaceS <rishavkumarcgc@gmail.com>
2023-03-12 18:14:07 +0530

    Added flight-create request middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const CityController = require('../../controllers/city-controller');
 const FlightController = require('../../controllers/flight-controller');
 const AirportController = require('../../controllers/airport-controller');
+const { FlightMiddlewares } = require('../../middlewares/index');
 const router = express.Router();
 
 router.post('/city', CityController.create); //POST : /api/v1/city
@@ -11,7 +12,11 @@ router.get('/city', CityController.getAll);
 router.patch('/city/:id', CityController.update); // PATCH : /api/v1/city/id
 
 router.get('/flights',FlightController.getAll); // get all the flights based on filters
-router.post('/flights', FlightController.create);
+router.post(
+    '/flights', 
+    FlightMiddlewares.validateCreateFlight, 
+    FlightController.create
+);
 
 router.post('/airports', AirportController.create);
 
